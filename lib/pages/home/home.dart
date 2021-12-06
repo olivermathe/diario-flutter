050b51348d2bc1b49aa7bb00e6dc8ecd0cf57764
Matheus Oliveira <olivermathe16@gmail.com>
2021-12-06 19:55:43 -0300

    aa

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,8 +1,6 @@
 import 'package:diario/pages/home/day_limit_presentation.dart';
-import 'package:diario/shared/models/config.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class Home extends StatefulWidget {
 
@@ -19,24 +17,21 @@ class HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => Config(),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Center(
-              child: DayLimitPresentation(
-                limit: 27.65,
-                spent: [
-                  10.00
-                ],
-              )
-          ),
-          Center(
-            child: Text('Baixo'),
-          ),
-        ],
-      ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Center(
+          child: DayLimitPresentation(
+            limit: 27.65,
+            spent: [
+              10.00
+            ],
+          )
+        ),
+        Center(
+          child: Text('Baixo'),
+        ),
+      ],
     );
   }
 
