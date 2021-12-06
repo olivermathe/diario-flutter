2b304275375761972a92136ffabe6321e2229c15
Matheus Oliveira <olivermathe16@gmail.com>
2021-12-06 20:18:32 -0300

    aa

--- lib/shared/mappers/mapper.dart
+++
@@ -1,6 +0,0 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-
-abstract class Mapper<T> {
-  T fromJson(DocumentSnapshot<Map<String, dynamic>> json) { throw Exception('Not implemented'); }
-  Map<String, dynamic> toJson(T model) { throw Exception('Not implemented'); }
-}
